a12642fffce0c60227a6bb2e00080c9cbf2b8812
andreabenevenuta <andrea@dataroots.io>
2023-11-23 15:12:11 +0100

    embellish post app and add extra_info field

--- applications/dataroots_recuitment.py
+++ applications/dataroots_recuitment.py
@@ -12,11 +12,21 @@ st.title(":iphone: Dataroots Posts Generator")
 st.sidebar.title('Customize your post')
 social_network = st.sidebar.selectbox('Select Social Network:', ['Instagram', 'LinkedIn'])
 position = st.sidebar.selectbox('Select Position:', ['Machine Learning Engineer', 'Data Engineer', 'Data Strategy'])
-tone = st.sidebar.selectbox('Select Tone:', ['Formal', 'Informal'])
-max_words = st.sidebar.slider('Max Number of Words', min_value=100, max_value=1000, value=200)
-temperature = st.sidebar.slider('Model temperature', min_value=0.0, max_value=1.0, value=0.5, step=0.01)
+tone = st.sidebar.selectbox('Select Tone:', ['Informal', 'Formal'])
+max_words = st.sidebar.slider('Max Number of Words', min_value=100, max_value=1000, value=200, step=10)
+extra_info = st.sidebar.text_area('Additional Information:', max_chars=500)
+
+temperature = st.sidebar.slider('Model temperature', min_value=0.0, max_value=1.0, value=0.0, step=0.01)
 
 # Generate button
 if st.sidebar.button('Generate'):
-    response = lch.generate_post_dataroots(social_network, position, tone, max_words, temperature, openai_api_key)
-    st.text(response['generated_post'])
+    response = lch.generate_post_dataroots(
+        openai_api_key=openai_api_key,
+        temperature=temperature, 
+        social_network=social_network, 
+        position=position, 
+        tone=tone, 
+        max_words=max_words,
+        extra_info=extra_info
+        )
+    st.text_area(label="Post", value=response['generated_post'], height=200)
--- applications/langchain_helper.py
+++ applications/langchain_helper.py
@@ -5,36 +5,36 @@ from langchain.chains import LLMChain
 
 
 
-def generate_post_dataroots(social_network, position, tone, max_words, temperature, openai_api_key):
+def generate_post_dataroots(openai_api_key, temperature, social_network, position, tone, max_words, extra_info= ""):
     llm = OpenAI(temperature=temperature, openai_api_key=openai_api_key)
 
     my_prompt = PromptTemplate(
-    input_variables = ["social_network", "position", "tone", "max_words"],
+    input_variables = ["social_network", "position", "tone", "max_words", "extra_info"],
     input_types={
         "social_network": str,
         "position": str,
         "tone": str,
-        "max_words": int
+        "max_words": int,
+        "extra_info": str
         },
     template = """
     You are Dataroots assistant and you are here to help HR create new posts on {social_network} to recruite new people.
     The post should be about looking for a {position}.
     The tone must be {tone}.
     The max number of words for the post should be {max_words}.
-    Be as captivating as possible and use emojis to convey the message.
+
+    {extra_info}
     """
     )
-
+#Be as captivating as possible and use emojis to convey the message.
     recruitment_chain = LLMChain(llm=llm, prompt=my_prompt, output_key="generated_post")
 
     response = recruitment_chain({
         "social_network": social_network,
         "position": position,
         "tone": tone,
-        "max_words": max_words
+        "max_words": max_words,
+        "extra_info": extra_info
     })
 
     return response
-
-if __name__ == "__main__":
-    print(generate_post_dataroots("Instagram", "Machine Learning Engineer", "informal", 200, 0))
