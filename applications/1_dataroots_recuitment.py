7b433ca4e75ab0e50190146f6760e712450edcff
andreabenevenuta <andrea@dataroots.io>
2023-12-01 10:00:52 +0100

    modify generate post application

--- applications/1_dataroots_recuitment.py
+++ applications/1_dataroots_recuitment.py
@@ -10,9 +10,9 @@ st.title(":iphone: Dataroots Posts Generator")
 
 # Sidebar select box
 st.sidebar.title('Customize your post')
-social_network = st.sidebar.selectbox('Select Social Network:', ['Instagram', 'LinkedIn'])
-position = st.sidebar.selectbox('Select Position:', ['Machine Learning Engineer', 'Data Engineer', 'Data Strategy'])
-tone = st.sidebar.selectbox('Select Tone:', ['Informal', 'Formal'])
+social_media = st.sidebar.selectbox('Social Media:', ['Instagram', 'LinkedIn'])
+position = st.sidebar.selectbox('Position:', ['Machine Learning Engineer', 'Data Engineer', 'Data Strategy'])
+tone = st.sidebar.selectbox('Tone:', ['Informal', 'Formal'])
 max_words = st.sidebar.slider('Max Number of Words', min_value=100, max_value=1000, value=200, step=10)
 extra_info = st.sidebar.text_area('Additional Information:', max_chars=500)
 
@@ -23,7 +23,7 @@ if st.sidebar.button('Generate'):
     response = lch.generate_post_dataroots(
         openai_api_key=openai_api_key,
         temperature=temperature, 
-        social_network=social_network, 
+        social_media=social_media, 
         position=position, 
         tone=tone, 
         max_words=max_words,
